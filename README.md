1a5784947545ef0358a15324a74b69e22c6f16d3
Alberto Coscia <alberto@albertocoscia.me>
2015-10-19 15:10:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Classeviva API
+Classeviva API is an unofficial Spaggiari's electronic register parser & REST API which translates the original spaghetti code of the service in something more readable --- JSON objects. It is built on top of NodeJS with CheerioJS and ExpressJS.
+## Features
+By now, Classeviva API can provide you:
+ * Login & session id
+ * Votes
+ * Agenda
+ * Files
+ 
+It obviously requires you to use a schooid, a user id and a password. Since Spaggiari is not able to support email and userid login in the same script, email login is not currently supported.
+## How does it work
+In a nutshell, the Node application makes requests to the original website, parses the HTML code and returns data in JSON format, that can be implemented in pretty much every application.
+## Endpoints
+ * `/custcode/userid/password`
+    * `custcode`: The school id. Do not ask me why Spaggiari has called it so.
+    * `userid`: The user id provided by Spaggiari.
+    * `password`: Does it even need an explanation?
+    * Checks if the password matches and returns the session id that you will need for every other operation
+    * Example output: `{"status":"OK", "sessionId":"<random alphanumeric 32chars-long string>"}`
+ * `/sessionId`
+    *  `sessionId`: The session id (hopefully, you obtained it with the previous endpoint)
+    *  Returns the user name and the school name (you can use it to check if the session id is still valid too)
+    *  Example output: `{"status":"OK","name":"FOO BAR","school":"FOOBAR Elementary School"}`
+ * `/sessionId/votes`
+    * Returns a JSON object containing all the user's vote
+    * Example output: `{"storia":[{"vote":"5","type":"Orale - 10/9"}],"inglese":[{"vote":"10","type":"Scritto/Grafico - 09/09"}]}`
+ * `/sessionId/agenda`
+    * Returns a JSON object containing the user's agenda from the previous day to the next week (will be changed to allow the use of custom timestamps)
+ * `/sessionId/files`
+    * Returns a JSON object containing all the files to which the user can access
+    * Example output: `{"Teacher Name":[{"name":"Folder name","list":[{"file":"File name","id":"numeric file id","url":"https://web.spaggiari.eu/cvv/app/default/didattica_genitori.php?a=downloadContenuto&contenuto_id=[fileid]"}]}]}`
+## Installation
+ * `git clone https://github.com/ALCC01/ClassevivaAPI.git`
+ * `cd ClassevivaAPI`
+ * `[sudo] npm install`
+ * `node app.js`
+## Contributing
+Pull requests are welcome.
+## License
+ClassevivaAPI is released under GNU General Public License 3.0.
+
+    Copyright (C) 2015 Alberto Coscia
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ app.js
@@ -0,0 +1,115 @@
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var app = express();
+var global_base_url = "https://web.spaggiari.eu/home/app/default/";
+var base_url = "https://web.spaggiari.eu/cvv/app/default/";
+
+app.get('/', function (req, res) {
+	res.send("No arguments provided")
+});
+
+app.get('/:custcode/:usercode/:password', function (req, res) {
+	var url = global_base_url + "login.php?custcode="+ req.params.custcode +"&login="+ req.params.usercode +"&password="+ req.params.password + "&mode=custcode";
+	var jar = request.jar();
+	request({url: url, jar: jar}, function(error, response, body) {
+		  $ = cheerio.load(body);
+		  if ($('.name').length) {
+		  	res.send('{"status":"OK", "sessionId":"' + jar.getCookies(url)[0].value + '"}');
+		  } else {
+		  	res.send('{"status":"error"}');
+		  }
+	});
+});
+
+app.get('/:sessionId/', function (req, res) {
+	var url = global_base_url + "menu_webinfoschool_genitori.php";
+	request({url: url, /*jar: jar*/ headers: {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:37.0) Gecko/20100101 Firefox/37.0',
+	            'Set-Cookie': "PHPSESSID=" + req.params.sessionId,
+	            'Cookie': "PHPSESSID=" + req.params.sessionId}
+	}, function(error, response, body) {
+		$ = cheerio.load(body);
+		if ($('.name').length) {
+			var result = new Object();
+			result['status'] = "OK";
+			result['name'] = $('.name').text();
+			result['school'] = $('.scuola').text();
+			res.send(JSON.stringify(result));
+		} else {
+			res.send('{"status":"error"}');
+		}
+	});
+});
+
+app.get('/:sessionId/votes', function (req, res) {
+	var url = base_url + "genitori_note.php";
+	request({url: url, /*jar: jar*/ headers: {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:37.0) Gecko/20100101 Firefox/37.0',
+	            'Set-Cookie': "PHPSESSID=" + req.params.sessionId,
+	            'Cookie': "PHPSESSID=" + req.params.sessionId}
+	}, function(error, response, body) {
+		$ = cheerio.load(body);
+		var votes = new Object();
+		var subject =  [];
+		$('#data_table_2 tr').not('#placeholder_row').each(function(i, e) {
+			if ($(this).children('td').first().text().match(/[a-z]/i)) {
+				var entry = $(this).children('td').first().text().replace(/(\n)/gm, "");
+				if (entry != subject) {
+					  votes[entry] = [];
+					  subject = entry;
+				}
+			} else {
+				var entry = $(this).children('td').eq(1).children('div').children('p').text();
+				var what = $(this).children('td').eq(1).children('p').text()
+				votes[subject].push({vote:entry, type:what});
+			}
+		});
+		res.send(votes);
+	});
+});
+
+app.get('/:sessionId/agenda', function (req, res) {
+	var url = base_url + "agenda_studenti.php?ope=get_events&classe_id=&gruppo_id=&start=" + parseInt(((new Date).getTime() / 1000 - 86400)) + "&end=" + parseInt(((new Date).getTime() / 1000 + 86400 * 7));
+	request({url: url, /*jar: jar*/ headers: {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:37.0) Gecko/20100101 Firefox/37.0',
+	            'Set-Cookie': "PHPSESSID=" + req.params.sessionId,
+	            'Cookie': "PHPSESSID=" + req.params.sessionId}
+	}, function(error, response, body) {
+		$ = cheerio.load(body);
+		res.send(body);
+	});
+});
+
+app.get('/:sessionId/files', function (req, res) {
+	var url = base_url + "didattica_genitori.php";
+	request({url: url, /*jar: jar*/ headers: {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:37.0) Gecko/20100101 Firefox/37.0',
+		'Set-Cookie': "PHPSESSID=" + req.params.sessionId,
+		'Cookie': "PHPSESSID=" + req.params.sessionId}
+	}, function(error, response, body) {
+		$ = cheerio.load(body);
+
+		var files = {};
+		var teacher = "";
+		$('#data_table tr').slice(9).each(function(i, e) {
+			var entry = $(this);
+			if (entry.attr('style') == "height: 40px;") {
+				teacher = entry.text().slice(21, -7);
+				files[teacher] = [];
+			}
+			if (entry.hasClass('row_parent')) {
+				var name = entry.text().slice(5, -4);
+				files[teacher].push({"name":name, "list":[]});
+			}
+			if (entry.hasClass('contenuto')) {
+				var name = entry.children('.contenuto_desc').children('div').children('span').eq(0).text()
+			  	files[teacher][files[teacher].length-1]['list'].push({"file":name, "id":entry.attr('contenuto_id'), "url":base_url + "didattica_genitori.php?a=downloadContenuto&contenuto_id=" + entry.attr('contenuto_id')});	
+			}
+		});
+		res.send(files);
+	});
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Classeviva API listening at http://%s:%s', host, port);
+});
